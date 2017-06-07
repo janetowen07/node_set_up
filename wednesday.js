52eae3e427dcd5139b03ab537b7eb686b47733df
Janet <janetowen07@gmail.com>
2017-06-07 17:13:20 +0100

    initial commit

---
+++ wednesday.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+// run npm mocha, chai, eslint, husky
+// update packageJSON test scripts
+
+fs.writeFile('index.js', 'module.exports =', (err) => {
+  if (err) throw err;
+  console.log('The index file has been saved!');
+});
+
+fs.writeFile('README.md', '', (err) => {
+  if (err) throw err;
+  console.log('The README file has been saved!');
+});
+
+fs.readFile('./.gitignore_template', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+  fs.writeFile('.gitignore', data, (err) => {
+    if (err) throw err;
+    console.log('The .gitignore file has been saved!');
+  });
+});
+
+fs.mkdir('./spec', (err) => {
+  if (err) throw err;
+  console.log('spec folder made');
+  fs.readFile('./testing.js', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    fs.writeFile('spec/spec.js', data, (err) => {
+      if (err) throw err;
+      console.log('The spec.js file has been saved');
+    });
+  });
+});
+
+const exec = require('child_process').exec;
+exec('npm init -y', (err, stdout) => {
+  console.log(stdout);
+  console.log('over');
+  
+});
